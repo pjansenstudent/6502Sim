b2cc8c88496cc3805c0f29d8bf83be3cc8fdd6e1
pjansenstudent <pjansen@lakeheadu.ca>
2022-02-09 21:25:27 -0500

    Implemented Memory class functions (before testing), comments still needed for documentation

--- 6502Sim/Memory.cpp
+++ 6502Sim/Memory.cpp
@@ -2,6 +2,29 @@
 /// <summary>
 /// Standard destructor class, clean up used memory to prevent memory leaks
 /// </summary>
+
+/// <summary>
+/// Default Constructor, will be unused in my case, and only throws an error when invoked at the moment
+/// </summary>
+Memory::Memory() {
+	throw 200; //placeholder error value, 
+}
+
+/// <summary>
+/// 
+/// </summary>
+/// <param name="memSize">Size of the Memory block to create, must be >= 2048 and <= 65536 </param>
+Memory::Memory(unsigned int memSize) {
+	if (memSize < 2048 || memSize > 65536) {
+		throw 4;
+	}
+	_memsize = memSize;
+
+	_memblock = new unsigned char[memSize]; //generate a 
+
+	clearMemory(); //clear the memory if it has anything in it
+}
+
 Memory::~Memory() {
 	delete[] _memblock; //de-allocate the memory used in the memory block
 }
@@ -13,4 +36,52 @@ void Memory::clearMemory() {
 	for (int i = 0; i < _memsize; i++) {
 		_memblock[i] = 0x00; //set binary value of memblock to 00000000
 	}
+}
+
+/// <summary>
+/// This Function will convert two characters (representing bits from the addressing pins of the processor)
+/// using this method adapted from https://stackoverflow.com/questions/25787349/convert-char-to-short/25787777
+/// My version is not using pointers, as it is unnecessary for the operation of this program
+/// </summary>
+/// <param name="offsetHigh">Offset for the high bytes of the memory address</param>
+/// <param name="offsetLow"></param>
+/// <returns></returns>
+unsigned short Memory::bytesToArrayOffset(unsigned char offsetHigh, unsigned char offsetLow) {
+	unsigned short retValue = 0x0000; //ensure that the variable is a clean 
+	retValue = offsetHigh;
+	retValue << 8; //shift bits left 8 places (for high bits of address)
+	retValue = retValue | offsetLow; //bitwise or function to add in the low 
+
+	return retValue;
+}
+
+/// <summary>
+/// This internal function checks to ensure that an address fed to the memory is within range
+/// </summary>
+/// <param name="addr">the address being checked</param>
+/// <returns></returns>
+bool Memory::checkAddress(unsigned short addr) {
+	if (addr > _memsize) {
+		return false; //if addr is bigger than _memsize, return false
+	}
+	return true; //otherwise return true
+}
+
+
+unsigned char Memory::read(unsigned char offsetHigh, unsigned char offsetLow) {
+	unsigned short addr = bytesToArrayOffset(offsetHigh, offsetLow);
+	if (!checkAddress(addr)) {
+		throw 5; //error code for invalid memory address
+	}
+	return _memblock[addr]; //return the data at that address
+}
+
+
+void Memory::write(unsigned char offsetHigh, unsigned char offsetLow, unsigned char value) {
+	unsigned short addr = bytesToArrayOffset(offsetHigh, offsetLow);
+	if (!checkAddress(addr)) {
+		throw 5; //error code for invalid memory address
+	}
+
+	_memblock[addr] = value; //write the value to the address
 }
--- 6502Sim/Memory.h
+++ 6502Sim/Memory.h
@@ -3,19 +3,21 @@
 /// <summary>
 /// This is the Memory Class, it will contain our memory, it really only needs a few functions, as it's job is to intialize a block of memory, then access or store memory based on an input binary address, and clear it when necessary
 /// I've decided to do address translation in this class, as it will 
+/// note the liberal use of unsigned, as we're dealing with raw binary (or in my case, hex, cause it's easier to work with)
 /// </summary>
 class Memory
 {
 private:
-	char* _memblock; //I'm using a char as it is a 8-bit variable, which we're going to use, since we're using 8 bit memory slots 
-	int _memsize; //a variable for storing the size of the memory
-	short bytesToArrayOffset(char offsetHigh, char offsetLow); //a function that will take care of address translation based on two 8-bit inputs, will be needed for addressing, since I can't just char/8 as
+	unsigned char* _memblock; //I'm using a char as it is a 8-bit variable, which we're going to use, since we're using 8 bit memory slots 
+	unsigned int _memsize; //a variable for storing the size of the memory
+	unsigned short bytesToArrayOffset(unsigned char offsetHigh, unsigned char offsetLow); //a function that will take care of address translation based on two 8-bit inputs, will be needed for addressing, since I can't just char/8 as
+	bool checkAddress(unsigned short addr);
 
 public:
 	Memory(); //default constructor which I will not be using in my case, but there for good practice
-	Memory(int memSize); //the actual constructor which we will use, 
+	Memory(unsigned int memSize); //the actual constructor which we will use, 
 	~Memory(); //our decstructor, to deal with our memory block on destruction
 	void clearMemory(); // a function for clearing the memory (aka: setting everything to 0x00) 
-	char read(char offsetHigh, char offsetLow);
-	void write(char offsetHigh, char offsetLow, char value);
+	unsigned char read(unsigned char offsetHigh, unsigned char offsetLow);
+	void write(unsigned char offsetHigh, unsigned char offsetLow, unsigned char value);
 };
